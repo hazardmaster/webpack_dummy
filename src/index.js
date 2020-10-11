f98677703285d53f8b5db164a0532cd6a43aceb9
Oscar Hazard <oscarkipkoech@gmail.com>
2020-10-11 19:42:30 +0300

    trying asset management different formats

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
+
+// Data
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
+
+  console.log(toml);
+  console.log(yaml);
+  console.log(json);
 
   return element;
 }
