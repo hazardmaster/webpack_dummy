c7c595dbb5ef34d19817896864b7bd34d850cb26
Oscar Hazard <oscarkipkoech@gmail.com>
2020-10-11 23:42:42 +0300

    Radical clean up

---
+++ README.md
@@ -0,0 +1,9 @@
+# webpack_dummy
+
+An absolute starting point on webpack
+
+## References
+
+https://webpack.js.org/guides/getting-started/
+
+<https://webpack.js.org/guides/asset-management>
--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-
-// Data
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
-
-  console.log(toml);
-  console.log(yaml);
-  console.log(json);
-
+  element.appendChild(btn);
   return element;
 }
 
